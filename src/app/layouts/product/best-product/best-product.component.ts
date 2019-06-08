6544df0e53874e0dedc1f8d6cc87527e68e0df70
Avery Knight <averjk@gmail.com>
2019-06-08 10:47:25 -0500

    solid product

--- src/app/layouts/product/best-product/best-product.component.ts
+++ src/app/layouts/product/best-product/best-product.component.ts
@@ -32,38 +32,49 @@ export class BestProductComponent implements OnInit {
 			responsive: {
 				'0': { items: 1, margin: 5 },
 				'430': { items: 2, margin: 5 },
-				'550': { items: 3, margin: 5 },
-				'670': { items: 4, margin: 5 }
+
 			},
 			autoplay: true,
 			loop: true,
-			autoplayTimeout: 3000,
+			autoplayTimeout: 6000,
 			lazyLoad: true
 		};
 		this.getAllProducts();
 	}
 
 	productLookup(name){
-    for(const val of this.bestProducts){
-      if(val.productName.toLowerCase() == name.toLowerCase()){
-        Client.newEventSession(Window["sessionID"]);
-        Client.track("view-product", JSON.stringify(val));
-        // this.http.post('https://api.reveldigital.com/api/devices/'+'4rdJw8N0r3Oso3SBsX2dbQ'+'/track?api_key=ZkgKeTifhhorIddmsChryA', {
-        //   "session_id": Window["sessionID"],
-        //   "event_name": "view-product",
-        //   "properties": val
-        // }).subscribe(()=>{
-        //
-        // });
-        this.router.navigate(['/products/product', val.$key]).then( (e) => {
-          if (e) {
-            this.app.tick();
-            console.log("Navigation is successful!");
-          } else {
-            console.log("Navigation has failed!");
-          }
-        });
-        break;
+	  if (name === "home") {
+
+      this.router.navigate(['/']).then( (e) => {
+        if (e) {
+          this.app.tick();
+          console.log("Navigation is successful!");
+        } else {
+          console.log("Navigation has failed!");
+        }
+      });
+    } else {
+      for (const val of this.bestProducts) {
+        if (val.productName.toLowerCase() == name.toLowerCase()) {
+          Client.newEventSession(Window["sessionID"]);
+          Client.track("view-product", JSON.stringify(val));
+          // this.http.post('https://api.reveldigital.com/api/devices/'+'4rdJw8N0r3Oso3SBsX2dbQ'+'/track?api_key=ZkgKeTifhhorIddmsChryA', {
+          //   "session_id": Window["sessionID"],
+          //   "event_name": "view-product",
+          //   "properties": val
+          // }).subscribe(()=>{
+          //
+          // });
+          this.router.navigate(['/products/product', val.$key]).then((e) => {
+            if (e) {
+              this.app.tick();
+              console.log("Navigation is successful!");
+            } else {
+              console.log("Navigation has failed!");
+            }
+          });
+          break;
+        }
       }
     }
   }
@@ -75,7 +86,7 @@ export class BestProductComponent implements OnInit {
 			(product) => {
 				this.loading = false;
 				this.bestProducts = [];
-				for (let i = 0; i < 5; i++) {
+				for (let i = 0; i < 20; i++) {
 					const y = product[i].payload.toJSON();
 					y['$key'] = product[i].key;
 					this.bestProducts.push(y as Product);
